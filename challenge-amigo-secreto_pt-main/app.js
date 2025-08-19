da41f9bac648caff06ddb1cac43d00dd21761ce8
Cristina Araújo <Crisbps@users.noreply.github.com>
2025-08-19 15:58:51 -0300

    versão final
    projeto pronto

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,5 +1,6 @@
 let amigos = []; // array para adicionar o nome para sorteio
 
+
 function adicionarAmigo() {
     let nomeDoAmigo = document.getElementById('amigo');
 
@@ -17,13 +18,33 @@ function adicionarAmigo() {
             console.log(amigos);
         }
     }
-        
+    listaDeAmigos()    
     limparCampo()
 }
+
+function listaDeAmigos() {
+        let lista = document.getElementById('listaAmigos');
+        lista.innerHTML = ''; // limpamos a lista 
+
+    for (let i = 0; i < amigos.length; i++) {
+       let li = document.createElement('li'); // criamos no HTML a tag <li> para receber os nomes da array
+        li.textContent = amigos[i];
+        lista.appendChild(li); // usamos a tag para colocar o <li> dentro do <ul>
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0){
+        document.getElementById('resultado').innerHTML = 'A lista de amigos está vazia.';
+        return // se a lista estiver vazia retornamos o texto acima
+    } 
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length); // sorteamos o nome aleatorio
+    let nomeSorteado = amigos[indiceAleatorio]; // guardamos em uma var
+    document.getElementById("resultado").innerHTML = "O amigo sorteado é: " + nomeSorteado; // e finalmente exibimos na tela
+}
+
 
 function limparCampo() {
     nomeDoAmigo = document.getElementById('amigo');
     nomeDoAmigo.value = '';
-}
-
-console.log(amigos);
+} // função para limpar o campo de texto
